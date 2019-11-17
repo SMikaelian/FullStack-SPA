fe0a59e1fad80480d98d273a25a7ddf5a4f493a2
SMikaelian <sarkis.mikaelian.712@my.csun.edu>
2019-11-17 15:17:44 -0800

    Initial commit

---
+++ backend/backend/Controllers/QuestionsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Questions")]
+    public class QuestionsController : Controller
+    {
+        readonly QuizContext context;
+        public QuestionsController(QuizContext context)
+        {
+            this.context = context; //reference to quiz context
+        }
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return context.Questions;
+            //return new Models.Question[]
+            //{
+            //    new Models.Question() {Text = "hello"},
+            //    new Models.Question() {Text = "hi"}
+
+            //};
+        }
+
+        [HttpPost]   
+        public async Task<IActionResult> Post([FromBody]Models.Question question)  //IActionResult Index() is same as HttpGet public void Post
+        {
+            context.Questions.Add(question);
+            //context.Questions.Add(new Models.Question() { Text = "test" });
+            await context.SaveChangesAsync();
+            return Ok(question);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]Models.Question question)
+        {
+            if (id != question.ID)
+                return BadRequest();
+            //var question = await context.Questions.SingleOrDefaultAsync(q => q.ID == id);
+            context.Entry(question).State = EntityState.Modified; //Telling the context that the question we get inside out question parameter once found set its state to modified and save changes
+            await context.SaveChangesAsync();
+            return Ok(question);
+        }
+
+    }
+}
---
+++ backend/backend/QuizContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend
+{
+    public class QuizContext : DbContext
+    {
+        public QuizContext(DbContextOptions<QuizContext> options) : base(options) { }
+        public DbSet<Models.Question> Questions { get; set; }
+
+    }
+}
